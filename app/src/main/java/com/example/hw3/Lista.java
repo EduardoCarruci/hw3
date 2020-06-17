0259f41db799ddddd67d21654dcd89af6f88b0a7
Germán <germanguevara2496@gmail.com>
2020-06-17 19:46:04 -0400

    ahi va lo que es el menu :v

--- app/src/main/java/com/example/hw3/Lista.java
+++ app/src/main/java/com/example/hw3/Lista.java
@@ -1,8 +1,12 @@
 package com.example.hw3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,5 +27,35 @@ public class Lista extends AppCompatActivity {
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,lista);
         list.setAdapter(adapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_lista, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        switch (itemId){
+
+            case R.id.action_add:
+                //AQUI EJECUTAS EL AÑADIR
+                return true;
+
+            case R.id.action_delete:
+                //AQUI EJECUTAS EL BORRAR
+                return true;
+
+            case R.id.action_sort:
+                //AQUI EJECUTAS EL ordenar
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
